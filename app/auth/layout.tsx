2b025ae86eaaf8fef98c2ab3d3ecfcbb0ebc965f
r3hmantom <r3hmantom@gmail.com>
2025-03-18 22:09:52 +0500

    Initialization

---
+++ app/auth/layout.tsx
@@ -0,0 +1,8 @@
+export default function AuthPageLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return <>{children}</>;
+  }
+  
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>dashboard page layout {children}</>;
+}
